edb38b83386a7c43bba7a1b062818d44e4be134c
kalyani23102001 <kalyanimaheshwari0@gmail.com>
2023-05-12 22:20:32 +0530

    Created a Project For Final Submission

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <span>Blog App</span>
+      <span>
+        Made by me and <b>React.js</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
